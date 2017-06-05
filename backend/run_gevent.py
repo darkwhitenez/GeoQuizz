4bf05ca892d0d32cd993e3f4a772732853b92410
njokisasirom <njokisasirom@users.noreply.github.com>
2017-06-05 13:14:50 +0200

    Use gevent monkey patching

--- backend/run_gevent.py
+++ backend/run_gevent.py
@@ -1,6 +1,9 @@
+from gevent import monkey
 from gevent.wsgi import WSGIServer
 
 from geoquizz import create_app
+
+monkey.patch_all()
 
 app = create_app()
 http_server = WSGIServer(('', 5000), app)
